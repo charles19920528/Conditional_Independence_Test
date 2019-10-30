0dfec45e9df85fc1dcf7bd55601e456d6bd1949f
Charles <charles19920528@hotmail.com>
2019-10-30 08:52:01 -0700

    Get some training results under the null. Saved plots and loss vectors.

--- main.py
+++ main.py
@@ -63,39 +63,110 @@ x_y_mat = np.random.binomial(n = 1, p = p_equal_1_mat, size = (sample_size_vet[0
 gt.log_ising_null(x_y_mat, p_equal_1_mat)
 """
 
+
 ####################
 # Network Training #
 ####################
-z = np.loadtxt("./data/z_100.txt", dtype = "float32")
-x_y_mat = np.loadtxt("./data/x_y_mat_100.txt", dtype = "float32")
+for training_sample_size in sample_size_vet:
+    z = np.loadtxt("./data/z_%d.txt" % training_sample_size, dtype="float32")
+    x_y_mat = np.loadtxt("./data/x_y_mat_%d.txt" % training_sample_size, dtype="float32")
+    p_equal_1_mat = np.loadtxt("./data/p_equal_1_mat_%d.txt" % training_sample_size, dtype="float32")
+
+    null_network_train = gt.IsingNetwork(dim_z, hidden_1_out_dim, 2)
+    null_network_train.dummy_run()
+    # optimizer = tf.keras.optimizers.SGD(learning_rate = 0.00001, momentum = True)
+    learning_rate = 0.005
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    iteration = 1600
+    train_loss = np.zeros(iteration)
+    kl_divergence = np.zeros(iteration)
+    for i in range(iteration):
+        with tf.GradientTape() as tape:
+            parameter_mat_pred = null_network_train(z)
+            loss = gt.log_ising_null(x_y_mat, parameter_mat_pred)
+        grads = tape.gradient(loss, null_network_train.variables)
+        optimizer.apply_gradients(grads_and_vars = zip(grads, null_network_train.variables))
+
+        parameter_mat_pred = null_network_train(z)
+        p_equal_1_hat_mat = gt.pmf_null(1, parameter_mat_pred)
+        kl_divergence[i] = np.sum(p_equal_1_mat * np.log(p_equal_1_mat / p_equal_1_hat_mat))
+        train_loss[i] = loss.numpy()
+
+        if i % 10 == 0:
+            print("Iteration %d, the loss is %f " % (i, loss))
+            print("The KL divergence is %f" % kl_divergence[i])
+
+
+    plt.figure(training_sample_size)
+    plt.plot(train_loss, label = "likelihood")
+    plt.plot(kl_divergence, label = "kl")
+    plt.legend()
+    plt.savefig("./figure/Loss function_%d.png" % training_sample_size)
+
+    np.savetxt("./results/train_loss_%d.txt" % training_sample_size, train_loss)
+    np.savetxt("./results/kl_%d.txt" % training_sample_size, kl_divergence)
+
+
+
+
+# Saved for graduate student descent
+
+training_sample_size = 30
+z = np.loadtxt("./data/z_%d.txt" % training_sample_size, dtype = "float32")
+x_y_mat = np.loadtxt("./data/x_y_mat_%d.txt" % training_sample_size , dtype = "float32")
+p_equal_1_mat = np.loadtxt("./data/p_equal_1_mat_%d.txt" % training_sample_size, dtype = "float32")
 
 null_network_train = gt.IsingNetwork(dim_z, hidden_1_out_dim, 2)
 null_network_train.dummy_run()
 #optimizer = tf.keras.optimizers.SGD(learning_rate = 0.00001, momentum = True)
-optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
 
-iteration = 1000
+iteration = 800
+
+# Let learning rate decay.
+"""
+step = tf.Variable(0, trainable = False)
+decay_times = int(iteration / 200)
+boundaries = np.linspace(0, iteration, num = decay_times + 1).tolist()
+values = np.geomspace(10 ** (-2), 10 ** (- decay_times - 3),
+                      num = decay_times + 2).tolist()
+learning_rate_fn = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries, values)
+learning_rate = learning_rate_fn(step)
+optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+"""
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.01)
+
+
+
 train_loss = np.zeros(iteration)
+kl_divergence = np.zeros(iteration)
 for i in range(iteration):
     with tf.GradientTape() as tape:
         parameter_mat_pred = null_network_train(z)
         loss = gt.log_ising_null(x_y_mat, parameter_mat_pred)
     grads = tape.gradient(loss, null_network_train.variables)
     optimizer.apply_gradients(grads_and_vars = zip(grads, null_network_train.variables))
+#    step = step + 1
 
+    parameter_mat_pred = null_network_train(z)
+    p_equal_1_hat_mat = gt.pmf_null(1, parameter_mat_pred)
+    kl_divergence[i] = np.sum(p_equal_1_mat * np.log(p_equal_1_mat / p_equal_1_hat_mat))
     train_loss[i] = loss.numpy()
 
     if i % 10 == 0:
         print("Iteration %d, the loss is %f " % (i, loss))
+        print("The KL divergence is %f" % kl_divergence[i])
 
 
-predicted_parameter = null_network_train(z)
-predicted_p_equal_1_mat = gt.pmf_null(1, predicted_parameter)
-
-p_equal_1_mat = np.loadtxt("./data/p_equal_1_mat_%d.txt" % 100)
-abs_difference = np.abs(predicted_p_equal_1_mat - p_equal_1_mat)
-plt.hist(abs_difference, normed = True, bins = 10)
 
 
-np.mean(np.abs(predicted_p_equal_1_mat - p_equal_1_mat))
-np.std(np.abs(predicted_p_equal_1_mat - p_equal_1_mat))
+
+
+plt.plot(train_loss, label = "likelihood")
+plt.plot(kl_divergence, label = "kl")
+plt.legend()
+plt.savefig("./figure/Loss function_%d.png" % training_sample_size)
+
+
+
