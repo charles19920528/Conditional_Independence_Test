98add311e0688e493fbab332b739f3f60727ce8b
Charles <charles19920528@hotmail.com>
2019-11-14 15:57:33 -0800

    Continue to work on my laptop.

--- building_isingsimulation.py
+++ building_isingsimulation.py
@@ -8,14 +8,14 @@ hidden_1_out_dim = 3
 
 # Create null network
 np.random.seed(1)
-null_network_generate = gt.IsingNetwork(dim_z, hidden_1_out_dim, 3)
+null_network_generate = gt.IsingNetwork(dim_z, hidden_1_out_dim, 2)
 null_network_generate.dummy_run()
 
 linear_1_weight_array = np.random.normal(1, 1, size=(dim_z, hidden_1_out_dim))
 linear_1_bias_array = np.zeros(shape=(hidden_1_out_dim,))
 
-linear_2_weight_array = np.random.normal(1, 1, size=(hidden_1_out_dim, 3))
-linear_2_bias_array = np.zeros(shape=(3,))
+linear_2_weight_array = np.random.normal(1, 1, size=(hidden_1_out_dim, 2))
+linear_2_bias_array = np.zeros(shape=(2,))
 
 null_network_generate.set_weights([linear_1_weight_array, linear_1_bias_array,
                                    linear_2_weight_array, linear_2_bias_array])
@@ -23,11 +23,14 @@ null_network_generate.set_weights([linear_1_weight_array, linear_1_bias_array,
 
 
 class IsingSimulation:
-    def __init__(self, z_mat, true_network, null_boolean,learning_rate, buffer_size, batch_size, epoch):
+    def __init__(self, z_mat, true_network, null_boolean, hidden_1_out_dim,learning_rate, buffer_size, batch_size,
+                 epoch):
         """
         Create a class which can generate data and train a network.
         :param z_mat: A n by p dimension numpy array / tensor. n is the sample size. This is the data we condition on.
         :param true_network: An object of IsingNetwork class. This is the true network we use to generate parameters.
+        :param null_boolean: A boolean value indicating if the true model is under the null.
+        :param hidden_1_out_dim: A scalar which is the output dimension of the hidden layer.
         :param learning_rate: A scalar which is a (hyper)parameter in the tf.keras.optimizers.Adam function.
         :param buffer_size: A scalar which is a (hyper)parameter in the tf.data.Dataset.shuffle function.
         :param batch_size: A scalar which is a (hyper)parameter in the tf.data.Dataset.batch function.
@@ -37,10 +40,13 @@ class IsingSimulation:
         self.sample_size = z_mat.shape[0]
         self.true_network = true_network
         self.null_boolean = null_boolean
+        self.hidden_1_out_dim = hidden_1_out_dim
         self.learning_rate = learning_rate
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.epoch = epoch
+
+        self.true_parameter = true_network(z_mat)
 
     def generate_x_y_mat(self):
         """
@@ -71,7 +77,100 @@ class IsingSimulation:
             return x_y_mat
 
     def trainning(self, x_y_mat):
-        pass
+        """
+        Train a neural network.
+        :param x_y_mat: An n x 2 numpy array. Each row is the response of the ith observation.
+        :return: result_dict: A dictionary which contains two keys which are "loss_array" and "jxy".
+        result_dict["loss_array"] is a 2 by epoch numpy of which the first row stores the (- 2 * LogLikelihood) and the
+        second row stores the kl divergences. result_dict["jxy"] stores a scalar which is the fitted value of Jxy in
+        the full Ising Model.
+        """
+        # Prepare training data.
+        train_ds = tf.data.Dataset.from_tensor_slices((self.z_mat, x_y_mat))
+        train_ds = train_ds.shuffle(self.buffer_size).batch(self.batch_size)
+
+        # Prepare storage for results.
+        loss_kl_array = np.zeros((2, self.epoch))
+        result_dict = dict()
+
+        train_network = gt.IsingNetwork(self.z_mat.shape[1], hidden_1_out_dim, 2)
+        optimizer = tf.keras.optimizers.Adam(learning_rate = self.learning_rate)
+
+        for i in range(self.epoch):
+            for z_batch, x_y_batch in train_ds:
+                with tf.GradientTape() as tape:
+                    batch_predicted_parameter_mat = train_network(z_batch)
+                    loss = gt.log_ising_null(x_y_batch, batch_predicted_parameter_mat)
+                grads = tape.gradient(loss, train_network.variables)
+                optimizer.apply_gradients(grads_and_vars=zip(grads, train_network.variables))
+
+                predicted_parameter_mat = train_network(self.z_mat)
+
+                batch_kl = gt.kl_divergence(self.true_parameter_mat, predicted_parameter_mat)
+
+
+            if i % 5 == 0:
+                print("Sample size %d, Epoch %d" % (self.sample_size, i))
+                print("The loss is %f " % loss)
+                print("The KL divergence is %f" % batch_kl)
+
+            if
+
+
+
+
+
+        loss_one_sample_dictionary[sample_size] = loss_kl_array
+
+
+        for sample_size in sample_size_vet:
+            # Produce raw data
+            z_mat = np.loadtxt("./data/z_%d.txt" % sample_size, dtype="float32")
+            true_parameter_mat = null_network_generate(z_mat)
+            p_equal_1_mat = gt.pmf_null(1, true_parameter_mat)
+            x_y_mat = np.random.binomial(n=1, p=p_equal_1_mat, size=(sample_size, 2)).astype("float32") * 2 - 1
+
+            train_network = gt.IsingNetwork(dim_z, hidden_1_out_dim, 2)
+            #    train_network.dummy_run()
+
+            # Hyperparameters for training.
+            learning_rate = 0.005
+            optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+            buffer_size = 1000
+            batch_size = 50
+            epoch = 1000
+
+            # Prepare training data.
+            train_ds = tf.data.Dataset.from_tensor_slices((z_mat, x_y_mat))
+            train_ds = train_ds.shuffle(buffer_size).batch(batch_size)
+
+            # Assume the first row corresponds to the likelihood
+            loss_kl_array = np.zeros((2, epoch * len(list(train_ds))))
+            loss_kl_array_index = 0
+            #    kl_divergence = np.zeros()
+            #    training_loss = np.zeros(epoch * len(list(train_ds)))
+
+            for i in range(epoch):
+                for z_batch, x_y_batch in train_ds:
+                    with tf.GradientTape() as tape:
+                        batch_predicted_parameter_mat = train_network(z_batch)
+                        loss = gt.log_ising_null(x_y_batch, batch_predicted_parameter_mat)
+                    grads = tape.gradient(loss, train_network.variables)
+                    optimizer.apply_gradients(grads_and_vars=zip(grads, train_network.variables))
+
+                    predicted_parameter_mat = train_network(z_mat)
+
+                    loss_kl_array[0, loss_kl_array_index] = loss.numpy()
+                    loss_kl_array[1, loss_kl_array_index] = gt.kl_divergence(true_parameter_mat,
+                                                                             predicted_parameter_mat)
+                    loss_kl_array_index += 1
+
+                if i % 5 == 0:
+                    print("Sample size %d, Epoch %d" % (sample_size, i))
+                    print("The loss is %f " % loss)
+                    print("The KL divergence is %f" % loss_kl_array[1, loss_kl_array_index - 1])
+
+            loss_one_sample_dictionary[sample_size] = loss_kl_array
 
 
 z_mat = np.loadtxt("./data/z_%d.txt" % 30, dtype="float32")
@@ -89,58 +188,3 @@ p_equal_1_mat = np.array([1,0, 0, 1]).reshape(2, 2)
 
 
 
-
-
-# See results for just one sample.
-loss_one_sample_dictionary = dict()
-
-for sample_size in sample_size_vet:
-    # Produce raw data
-    z_mat = np.loadtxt("./data/z_%d.txt" % sample_size, dtype="float32")
-    true_parameter_mat = null_network_generate(z_mat)
-    p_equal_1_mat = gt.pmf_null(1, true_parameter_mat)
-    x_y_mat = np.random.binomial(n=1, p=p_equal_1_mat, size=(sample_size, 2)).astype("float32") * 2 - 1
-
-    train_network = gt.IsingNetwork(dim_z, hidden_1_out_dim, 2)
-    train_network.dummy_run()
-
-    # Hyperparameters for training.
-    learning_rate = 0.005
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    buffer_size = 1000
-    batch_size = 50
-    epoch = 1000
-
-    # Prepare training data.
-    train_ds = tf.data.Dataset.from_tensor_slices((z_mat, x_y_mat))
-    train_ds = train_ds.shuffle(buffer_size).batch(batch_size)
-
-    # Assume the first row corresponds to the likelihood
-    loss_kl_array = np.zeros((2, epoch * len(list(train_ds))))
-    loss_kl_array_index = 0
-    #    kl_divergence = np.zeros()
-    #    training_loss = np.zeros(epoch * len(list(train_ds)))
-
-    for i in range(epoch):
-        for z_batch, x_y_batch in train_ds:
-            with tf.GradientTape() as tape:
-                batch_predicted_parameter_mat = train_network(z_batch)
-                loss = gt.log_ising_null(x_y_batch, batch_predicted_parameter_mat)
-            grads = tape.gradient(loss, train_network.variables)
-            optimizer.apply_gradients(grads_and_vars=zip(grads, train_network.variables))
-
-            predicted_parameter_mat = train_network(z_mat)
-
-            loss_kl_array[0, loss_kl_array_index] = loss.numpy()
-            loss_kl_array[1, loss_kl_array_index] = gt.kl_divergence(true_parameter_mat, predicted_parameter_mat)
-            loss_kl_array_index += 1
-
-        if i % 5 == 0:
-            print("Sample size %d, Epoch %d" % (sample_size, i))
-            print("The loss is %f " % loss)
-            print("The KL divergence is %f" % loss_kl_array[1, loss_kl_array_index - 1])
-
-    loss_one_sample_dictionary[sample_size] = loss_kl_array
-
-with open("./results/loss_one_sample_dictionary.p", "wb") as fp:
-    pickle.dump(loss_one_sample_dictionary, fp, protocol=pickle.HIGHEST_PROTOCOL)
