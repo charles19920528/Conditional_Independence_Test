029317280ee8941044b525be8b1aaaa8ffe7f24d
xw <charles19920528@hotmail.com>
2022-03-26 18:52:05 -0700

    Amend wald test statistic method. Didn't solve rank deficiency.

--- test_statistic.py
+++ test_statistic.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import generate_train_functions as gt
 
+
 def bias_dot(x_without_bias_mat: np.array, weights_with_bias_vet: np.array):
     """
     Compute X Beta + bias.
@@ -14,9 +15,11 @@ def bias_dot(x_without_bias_mat: np.array, weights_with_bias_vet: np.array):
     """
     return x_without_bias_mat.dot(weights_with_bias_vet[:-1]) + weights_with_bias_vet[-1]
 
+
 # x_mat = np.arange(6).reshape(2, 3)
 # weights = np.array([1, 1, 0, 10])
 # bias_dot(x_mat, weights)
+
 
 def theta_derivatives(x_y_mat, final_linear_input_mat, j_mat, gradient_boolean, hessian_boolean):
     if not (gradient_boolean or hessian_boolean):
@@ -31,9 +34,13 @@ def theta_derivatives(x_y_mat, final_linear_input_mat, j_mat, gradient_boolean, 
     if gradient_boolean:
         x_times_y_vet = x_y_mat[:, 0] * x_y_mat[:, 1]
         multiplier_vet = 2 * (e_jx_p_jy_vet + 1) / e1_denominator_vet - x_times_y_vet - 1
+        multiplier_vet = multiplier_vet.reshape(-1, 1)
         # n by p
-        gradient_mat = final_linear_input_mat * multiplier_vet.reshape(-1, 1)
+        gradient_mat = final_linear_input_mat * multiplier_vet
+        gradient_mat = np.hstack([gradient_mat, multiplier_vet])
         result_list.append(gradient_mat)
+
+        del multiplier_vet
 
     if hessian_boolean:
         cosh_sum_vet = np.cosh(2 * j_mat[:, 0]) + np.cosh(2 * j_mat[:, 1])
@@ -49,12 +56,11 @@ def theta_derivatives(x_y_mat, final_linear_input_mat, j_mat, gradient_boolean, 
                     second_derivative_vet = second_derivative_vet * final_linear_input_mat[:, i]
                 if j != hessian_mat.shape[0] - 1:
                     second_derivative_vet = second_derivative_vet * final_linear_input_mat[:, j]
-                hessian_mat[i, j] = hessian_mat[j, i] = second_derivative_vet.sum()
+                hessian_mat[i, j] = hessian_mat[j, i] = second_derivative_vet.mean()
 
         result_list.append(hessian_mat)
 
     return result_list
-
 
 
 class WaldTest:
@@ -63,22 +69,32 @@ class WaldTest:
         self.z_mat = z_mat
 
         self.network_test = network_test
-        self.network_test.call(x_y_mat[0, :])
+        self.network_test.call(z_mat[0:2, :])
         self.network_test.set_weights(network_weights_vet)
 
-        self.theta_vet = np.concatenate([network_weights_vet[-2][:, 2], [network_weights_vet[-1][2]]])
+        self.theta_vet = np.concatenate([network_weights_vet[-2][:, 2], [network_weights_vet[-1][2]]]).reshape(-1, 1)
 
-        self.j_mat = self.network_test.call(z_mat)
+        self.j_mat = self.network_test.call(z_mat).numpy()
         self.final_linear_input_mat = self.network_test.final_linear_input_mat
 
         self.test_statistic = None
 
     def _compute_test_statistic(self):
-        pass
-        # self.gradient =
+        self.gradient_mat, hessian_mat = theta_derivatives(x_y_mat=self.x_y_mat,
+                                                           final_linear_input_mat=self.final_linear_input_mat,
+                                                           j_mat=self.j_mat, gradient_boolean=True,
+                                                           hessian_boolean=True)
+        self.inverse_hessian_mat = np.linalg.solve(hessian_mat, np.identity(hessian_mat.shape[0]))
+        gradient_cov_mat = np.cov(self.gradient_mat.T, ddof=0)
+        meat_mat = self.inverse_hessian_mat.dot(gradient_cov_mat).dot(self.inverse_hessian_mat)
+        meat_inverse_mat = np.linalg.solve(meat_mat, np.identity(meat_mat.shape[0]))
+        self.test_statistic = self.theta_vet.T.dot(meat_inverse_mat).dot(self.theta_vet)
 
     def get_test_statistic(self):
         if self.test_statistic is None:
             self._compute_test_statistic()
         else:
             return self.test_statistic
+
+# def is_pos_def(x):
+#     return np.all(np.linalg.eigvals(x) > 0)
